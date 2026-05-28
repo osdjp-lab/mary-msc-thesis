402e16d03174a3f23afb432f51bce1928cefc1ea
Oscar Szumiak <oscarszumiak@gmail.com>
2026-05-28 17:51:09 +0200

    Corrections to data processing

--- data/2-fixed.sh
+++ data/2-fixed.sh
@@ -11,5 +11,33 @@ done
 
 echo $(find $OUTPUT_DIR -type f)
 
-sed -i -e 's/"/'\''/g' -e 's/^/"/g' -e 's/$/"/g' -e 's/;/,/g' $(find $OUTPUT_DIR -type f)
+# Replace non standard double quotes
+
+sed -i -e 's/“/"/g'  -e 's/”/"/g'  -e 's/«/"/g'  -e 's/»/"/g'  \
+       -e 's/“/"/g'  -e 's/”/"/g'  -e 's/„/"/g'  -e 's/‟/"/g'  \
+       -e 's/‹/"/g'  -e 's/›/"/g'  -e 's/「/"/g' -e 's/」/"/g' \
+       -e 's/『/"/g' -e 's/』/"/g' -e 's/〝/"/g' -e 's/〞/"/g' \
+       -e 's/〟/"/g' -e 's/＂/"/g' -e 's/｢/"/g'  -e 's/｣/"/g'  \
+       -e 's/❝/"/g'  -e 's/❞/"/g'  \
+       $(find $OUTPUT_DIR -type f)
+
+# Replace non standard single quotes
+
+sed -i -e "s/’/'/g"  -e "s/’/'/g" -e "s/‘/'/g"  -e "s/’/'/g"  \
+       -e "s/‚/'/g"  -e "s/＇/'/g" -e "s/❛/s'/g" -e "s/❜/'/g" \
+       $(find $OUTPUT_DIR -type f)
+
+# Remove prexisting double quotes
+
+sed -i -e 's/"/'\''/g' $(find $OUTPUT_DIR -type f)
+
+# Strip leading and trailing spaces from fields and add delimiting quotes
+
+sed -i -e 's/^\s*/"/g'    \
+       -e 's/\s*$/"/g'    \
+       $(find $OUTPUT_DIR -type f)
+
+# Replace occurences of interfield seperator in the fields with comma
+
+sed -i -e 's/;/,/g' $(find $OUTPUT_DIR -type f)
 
