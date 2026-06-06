a402572c9f52291e98184ba508b20f490f1d7969
Oscar Szumiak <oscarszumiak@gmail.com>
2026-06-06 19:39:27 +0200

    Split out only results for people taking suplements

--- data/run_all.sh
+++ data/run_all.sh
@@ -2,11 +2,53 @@
 
 # Run all scripts in order
 
-cd 1-split
-awk -f ../1-split.awk ../0-raw/results.csv
+OUTPUT_DIR='org'
+
+mkdir -p $OUTPUT_DIR
+
+mkdir $OUTPUT_DIR/1-split
+
+cd $OUTPUT_DIR/1-split
+awk -f ../../1-split.awk ../../raw/results.csv
 cd -
-./2-fixed.sh 1-split 2-fixed
-./3.1-values.sh 2-fixed 3.1-values
-./3.2-groups.sh 2-fixed 3.2-groups
-./4.1-reg-counts.sh 3.1-values 2-fixed 4.1-reg-counts
+
+mkdir -p $OUTPUT_DIR/2-fixed
+./2-fixed.sh $OUTPUT_DIR/1-split $OUTPUT_DIR/2-fixed
+
+mkdir -p $OUTPUT_DIR/3.1-values
+./3.1-values.sh $OUTPUT_DIR/2-fixed $OUTPUT_DIR/3.1-values
+
+mkdir -p $OUTPUT_DIR/3.2-groups
+./3.2-groups.sh $OUTPUT_DIR/2-fixed $OUTPUT_DIR/3.2-groups
+
+mkdir -p $OUTPUT_DIR/4.1-reg-counts
+./4.1-reg-counts.sh $OUTPUT_DIR/3.1-values $OUTPUT_DIR/2-fixed $OUTPUT_DIR/4.1-reg-counts
+
+mkdir -p $OUTPUT_DIR/4.2-alt-counts
+./4.2-alt-counts.sh $OUTPUT_DIR/2-fixed $OUTPUT_DIR/3.1-values $OUTPUT_DIR/4.2-alt-counts
+
+OUTPUT_DIR='only-yes'
+
+mkdir -p $OUTPUT_DIR
+
+mkdir $OUTPUT_DIR/1-split
+
+cd $OUTPUT_DIR/1-split
+awk -f ../../1-split.awk ../../raw/results-only-yes.csv
+cd -
+
+mkdir -p $OUTPUT_DIR/2-fixed
+./2-fixed.sh $OUTPUT_DIR/1-split $OUTPUT_DIR/2-fixed
+
+mkdir -p $OUTPUT_DIR/3.1-values
+./3.1-values.sh $OUTPUT_DIR/2-fixed $OUTPUT_DIR/3.1-values
+
+mkdir -p $OUTPUT_DIR/3.2-groups
+./3.2-groups.sh $OUTPUT_DIR/2-fixed $OUTPUT_DIR/3.2-groups
+
+mkdir -p $OUTPUT_DIR/4.1-reg-counts
+./4.1-reg-counts.sh $OUTPUT_DIR/3.1-values $OUTPUT_DIR/2-fixed $OUTPUT_DIR/4.1-reg-counts
+
+mkdir -p $OUTPUT_DIR/4.2-alt-counts
+./4.2-alt-counts.sh $OUTPUT_DIR/2-fixed $OUTPUT_DIR/3.1-values $OUTPUT_DIR/4.2-alt-counts
 
