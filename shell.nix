d7a07380e8114f90cb2e1ea48d9555fea2481fc1
Oscar Szumiak <oscarszumiak@gmail.com>
2026-06-06 23:52:28 +0200

    Merged shell.nix files

--- shell.nix
+++ shell.nix
@@ -1,9 +1,24 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-  buildInputs = [
+  packages = [
+    # Spell‑checking with English dictionary
     (pkgs.aspellWithDicts (d: with d; [ en ]))
+
+    # Full TeX Live distribution
     pkgs.texlive.combined.scheme-full
+
+    # GNU Make
     pkgs.gnumake
+
+    # Python 3 with scientific packages
+    (pkgs.python3.withPackages (ps: with ps; [
+      numpy
+      pandas
+      matplotlib
+    ]))
+
+    # csvkit utilities
+    pkgs.csvkit
   ];
 }
